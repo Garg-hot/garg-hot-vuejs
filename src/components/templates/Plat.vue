b3ec9ec55181b4e49a16a11b872e4d55e1fc0dd8
fanouangela15 <rf.angela015@gmail.com>
2025-02-10 07:43:18 +0300

    Plat style

--- src/components/templates/Plat.vue
+++ src/components/templates/Plat.vue
@@ -390,5 +390,17 @@ button:hover {
 .edit-form {
   animation: slideIn 0.3s ease-out;
 }
+/* Ajoutez ceci dans votre section style */
+* {
+  color: #000; /* Définit la couleur de texte noire pour tous les éléments */
+}
+
+h1, h2, h3, p, input, textarea, select {
+  color: #000; /* S'assure que le texte dans les en-têtes, paragraphes, et champs de saisie est noir */
+}
+
+textarea, input[type="text"], input[type="number"], select {
+  color: #000; /* S'assure que le texte dans les champs de saisie est noir */
+}
 
 </style>
